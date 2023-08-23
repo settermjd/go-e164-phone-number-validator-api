c77ca292055dedbeaf8ddecc58160fdbcd1ff25a
Matthew Setter <matthew@matthewsetter.com>
2023-08-23 12:49:42 +0200

    Refactor the app into a web-based application
    Using Go's net/http package, the application has been refactored into a
    web-based application, a simplistic API, that accepts requests to one
    route, where a phone number is provided and validated. The confirmation
    to the user of the validity of the number is simplistically returned as
    the response's body.

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 	"regexp"
 	"strings"
 )
@@ -13,5 +16,28 @@ func IsValidPhoneNumber(phone_number string) bool {
 	return re.Find([]byte(phone_number)) != nil
 }
 
+func validate(w http.ResponseWriter, r *http.Request) {
+	phone_number := r.FormValue("phone_number")
+	if phone_number == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("No phone number was provided\n"))
+		return
+	}
+
+	if IsValidPhoneNumber(phone_number) {
+		w.Write([]byte(fmt.Sprintf("[%s] is a valid phone number", phone_number)))
+		return
+	}
+
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(fmt.Sprintf("[%s] is NOT a valid phone number", phone_number)))
+}
+
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", validate)
+
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
